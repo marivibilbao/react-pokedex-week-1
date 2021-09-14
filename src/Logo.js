db7746be01a5831acd9c860caec7c33692ffae72
Maria Victoria <marivibilbaog@gmail.com>
2021-09-14 22:18:43 +0200

    Agrego ejercicio A en clase

--- src/Logo.js
+++ src/Logo.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Logo = (props) => {
+  const logWhenClicked = () =>{
+    console.log("Cualquier cosa");
+  };
   //const welcomeMessage = "Welcome to the Pokedex";
   const {appName} = props;
-  console.log(appName);
+  //console.log(appName);
   return (
     <header>
       <h1>{appName}</h1>
-      <img
+      <img onClick={logWhenClicked}
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="Pokemon"
       />
